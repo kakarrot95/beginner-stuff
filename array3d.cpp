bf16c1c5ed3fec9d10ffa64096b8e1d1508722c9
Anuj Verma <anujverma3563@gmail.com>
2023-11-04 11:19:52 +0530

    array memory manupulation

---
+++ array3d.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//
+//int main()
+//{
+//
+//	int*** a3d = new int** [50];
+//	for (int i = 0; i < 50; i++)
+//	{
+//		a3d[i] = new int* [i + 1];
+//		for (int ii = 0; ii < i + 1; ii++)
+//		{
+//			a3d[i][ii] = new int[ii + 1];
+//		}
+//	}
+//
+//
+//
+//	std::cin.get();
+//}
